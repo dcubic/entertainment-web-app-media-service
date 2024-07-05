e2f3efc0bf5e1c7f48280a4cf22fdc412ecf7170
unknown <dominikcubic@gmail.com>
2024-07-05 13:05:02 -0700

    :lock: added cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from 'cors'
 import { MediaRouter } from "../routers/MediaRouter";
 import { checkJWTValidity, handleErrors } from "../utils/middleware";
 import HealthRouter from "../routers/HealthRouter";
 
 export const createApp = () => {
   const app = express();
+  app.use(cors());
   const mediaRouter = new MediaRouter();
   const healthRouter = new HealthRouter();
 
